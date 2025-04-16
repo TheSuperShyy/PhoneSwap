478e191d3fc4e4783ae3421f5a8d4cae145c08fc
TheSuperShyy <yulgatch123@gmail.com>
2025-04-16 16:20:37 +0800

    Update dbcon.php

--- dbcon/dbcon.php
+++ dbcon/dbcon.php
@@ -2,14 +2,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 date_default_timezone_set('Asia/Manila');
 
-
 use MongoDB\Client;
 
 // Connect to MongoDB
 $client = new Client("mongodb://localhost:27017");
 $db = $client->selectDatabase('phoneSwap');
+
+// Collections
 $usersCollection = $db->selectCollection('users');
 $phonesCollection = $db->selectCollection('phones');
 $phoneauditCollection = $db->selectCollection('phone_audit');
-
+$phoneswapauditCollection = $db->selectCollection('phone_swap_audit');
 ?>
